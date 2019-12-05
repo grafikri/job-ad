99b2fbc9174e45000109378cc11a6b982b9c17ce
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 11:53:12 +0300

    Ad component template added

---
+++ src/components/templates/VAd/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <VPost v-bind="post" />
+  </div>
+</template>
+
+<script>
+
+import VPost from '../../molecules/VPost/index'
+
+export default {
+  name: "VAd",
+  components: {
+    VPost
+  },
+  props: {
+    post: Object
+  }
+}
+</script>
+
+<style>
+</style>
