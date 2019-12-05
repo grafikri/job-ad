bb8c73dc66cdc5385fb7e23b29d88b231542570a
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 16:19:25 +0300

    actions method created I added setTimeout method to simulate it that as if comes from API

--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,27 +1,35 @@
+import data from "../../assets/fakeData/posts.json"
+
 /**
  * This state is used to show post list
  */
 const state = {
-  posts: [
-    {
-      id: "1",
-      jobTitle: "Gerber",
-      companyName: "Testoğlu Pide",
-      duration: "Taze çıktı",
-      distance: "8.53 yakınında",
-      cityName: "İstanbul",
-      town: "Çengelköy"
-    },
-    {
-      id: "2",
-      jobTitle: "Gerber 2",
-      companyName: "Testoğlu Pide",
-      duration: "Taze çıktı",
-      distance: "8.53 yakınında",
-      cityName: "İstanbul",
-      town: "Çengelköy"
-    }
-  ]
+  posts: []
+}
+
+// post:[{
+//   id: "1",
+//   jobTitle: "Gerber",
+//   companyName: "Testoğlu Pide",
+//   duration: "Taze çıktı",
+//   distance: "8.53 yakınında",
+//   cityName: "İstanbul",
+//   town: "Çengelköy"
+// },
+// {
+//   id: "2",
+//   jobTitle: "Gerber 2",
+//   companyName: "Testoğlu Pide",
+//   duration: "Taze çıktı",
+//   distance: "8.53 yakınında",
+//   cityName: "İstanbul",
+//   town: "Çengelköy2"
+// }]
+
+const mutations = {
+  addAll: (state, payload) => {
+    state.posts = payload
+  }
 }
 
 const getters = {
@@ -30,8 +38,21 @@ const getters = {
   }
 }
 
+const actions = {
+  fetchAll(context) {
+    /**
+     * Simulating to fetching data as if it comes from API
+     */
+    setTimeout(() => {
+      context.commit("addAll", data)
+    }, 1500)
+  }
+}
+
 export default {
   namespaced: true,
   getters,
+  mutations,
+  actions,
   state
 }
