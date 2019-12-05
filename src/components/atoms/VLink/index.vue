ad9952cfb0ca36b9bbabda880a68cf76d5c485b4
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 13:53:27 +0300

    Link component added

---
+++ src/components/atoms/VLink/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="href">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+
+export default {
+  name: "VLink",
+  props: {
+    href: {
+      type: String,
+      default: "#"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
