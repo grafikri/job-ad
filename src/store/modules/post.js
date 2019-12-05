9379117f7090dfd50ede329ec0d864ab9b6d854c
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 17:51:37 +0300

    mutations and actions methods added

--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,44 +1,43 @@
 /**
+ * data is represented API response
+ */
+import data from "../../assets/fakeData/posts.json"
+
+/**
  * This state is used to show post detail
  */
 const state = {
-  jobId: "199A9ED66BFE4CF593DBDF5402F1702A",
-  positionName: "Jandarma Bölge Komutanı",
-  companyName: "Vosyoyvsyvosyv",
-  durationDay: 0,
-  durationDayText: "Taze çıktı!",
-  imageUrl: "https://localhost/bluecollar/company/job/Default/6.jpg",
-  countryCode: "TR",
-  countryName: "Turkey",
-  cityName: "İstanbul",
-  townName: "Ümraniye",
-  address:
-    "Saray Mahallesi Site Yolu Sokak No:7 34768 Ümraniye/İstanbul Turkey",
-  postalCode: 34768,
-  hasLatitude: true,
-  hasLongitude: true,
-  latitude: 41.0266502,
-  longitude: 29.1157803,
-  description: "syovsvyosvyoyvsoby",
-  status: 1,
-  contactPhone: {
-    countryCallingCode: "+90",
-    areaCode: "879",
-    number: "9797979"
-  }
+  post: {}
 }
 
 const getters = {
   purePost: state => {
     return {
-      jobTitle: state.positionName,
-      company: state.companyName
+      jobTitle: state.post.positionName,
+      company: state.post.companyName
     }
+  }
+}
+
+const mutations = {
+  updateAll(state, payload) {
+    state.post = payload
+  }
+}
+
+const actions = {
+  fetchPost({ commit, state }, payload) {
+    let post = data.result.items
+      .filter(item => item.jobId == payload.id)
+      .shift()
+    commit("updateAll", post)
   }
 }
 
 export default {
   namespaced: true,
   getters,
+  actions,
+  mutations,
   state
 }
