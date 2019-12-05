b343a9577e583469591a896a4b73e573734a1105
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 13:54:06 +0300

    Home component template added

---
+++ src/components/templates/VHome/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <VLink href="/joblist">
+      <VButton value="Tüm ilanları listele" />
+    </VLink>
+  </div>
+</template>
+
+<script>
+
+import VButton from '../../atoms/VButton/index'
+import VLink from '../../atoms/VLink/index'
+
+export default {
+  name: "VHome",
+  components: {
+    VButton,
+    VLink
+  }
+}
+</script>
+
+<style>
+</style>
