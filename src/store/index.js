7acc9730f6c227c988b05a4465b2b1783938e94f
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 12:22:54 +0300

    posts modules was added to main store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
+import posts from "./modules/posts"
+
 export default new Vuex.Store({
   state: {
+    appName: "Everywhere is Ads"
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations: {},
+  actions: {},
   modules: {
+    posts
   }
 })
