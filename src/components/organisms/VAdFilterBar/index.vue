8b00de8a0ed41685d06cbfcc79816b4fec4325aa
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 22:12:33 +0300

    filter bar added to find ads

---
+++ src/components/organisms/VAdFilterBar/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="v-o-ad-filter-bar">
+    <b-form inline v-on:submit.prevent="onSubmit" action="#">
+      <b-input
+        id="inline-form-input-name"
+        class="mb-2 mr-sm-2 mb-sm-0"
+        placeholder="Konum"
+        v-model="location"
+      ></b-input>
+      <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inline-form-input-username" placeholder="Pozisyon, ünvan" v-model="title"></b-input>
+      </b-input-group>
+      <b-button type="submit" variant="primary" :disabled="isDisabledSubmit">Listele</b-button>
+      <b-button variant="danger" class="btn-reset" @click="formReset">Sıfırla</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VAdFilterBar",
+  data: () => {
+    return {
+      location: "",
+      title: "çengelköy",
+      isDisabledSubmit: false
+    }
+  },
+  props: {
+    submit: Function,
+    reset: Function
+  },
+  watch: {
+    location(val) {
+      this.isDisabledSubmit = val.length > 0 || this.title.length > 0 ? false : true
+    },
+    title(val) {
+      this.isDisabledSubmit = val.length > 0 || this.location.length > 0 ? false : true
+    },
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("submit", {
+        location: this.location,
+        title: this.title
+      })
+    },
+    formReset() {
+      this.location = ""
+      this.title = ""
+      this.$emit("reset")
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+  .v-o-ad-filter-bar
+    padding: 10px
+    background-color: $light
+    border-bottom: 0.5px solid $secondary
+    .btn-reset
+      margin-left: 10px
+
+</style>
