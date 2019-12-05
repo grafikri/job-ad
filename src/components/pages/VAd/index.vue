10d7fb412bd697ce61a011579d5a9cd3ae9461e2
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 11:53:19 +0300

    ad page added

---
+++ src/components/pages/VAd/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <VAdTemplate :post="post" />
+  </div>
+</template>
+
+<script>
+
+import VAdTemplate from '../../templates/VAd/index'
+
+export default {
+  name: "VAd",
+  components: {
+    VAdTemplate
+  },
+  data: () => {
+    return {
+      post: {
+        position: "Hello World",
+        jobTitle: "Full Stack Developer"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
