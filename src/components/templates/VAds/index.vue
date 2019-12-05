5622da9ae7159bf4156c3ecac8add0252e8e5ae6
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 11:21:13 +0300

    ads template component added

---
+++ src/components/templates/VAds/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <VPosts :items="items" />
+  </div>
+</template>
+
+<script>
+
+import VPosts from "../../organisms/VPosts/index.vue"
+
+export default {
+  name: "VAds",
+  components: {
+    VPosts
+  },
+  data: () => {
+    return {
+      items: [
+        {
+          jobTitle: "Gerber",
+          companyName: "Testoğlu Pide",
+          duration: "Taze çıktı",
+          distance: "8.53 yakınında",
+          cityName: "İstanbul",
+          town: "Çengelköy"
+        },
+        {
+          jobTitle: "Gerber 2",
+          companyName: "Testoğlu Pide",
+          duration: "Taze çıktı",
+          distance: "8.53 yakınında",
+          cityName: "İstanbul",
+          town: "Çengelköy"
+        },
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
