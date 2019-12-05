9b5fcf2d013724837e1828f8591258d801607225
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 11:28:10 +0300

    ads pages added

---
+++ src/components/pages/VAds/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <VAdsTemplate :posts="posts" />
+  </div>
+</template>
+
+<script>
+
+import VAdsTemplate from '../../templates/VAds/index.vue'
+
+export default {
+  name: "VAds",
+  components: {
+    VAdsTemplate
+  },
+  data: () => {
+    return {
+      posts: [
+        {
+          jobTitle: "Gerber",
+          companyName: "Testoğlu Pide",
+          duration: "Taze çıktı",
+          distance: "8.53 yakınında",
+          cityName: "İstanbul",
+          town: "Çengelköy"
+        },
+        {
+          jobTitle: "Gerber 2",
+          companyName: "Testoğlu Pide",
+          duration: "Taze çıktı",
+          distance: "8.53 yakınında",
+          cityName: "İstanbul",
+          town: "Çengelköy"
+        },
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
