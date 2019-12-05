caf20a8b52ee1f423517c3e388161d9bca26f5f2
Serhan Coşkun <grafikri@gmail.com>
2019-12-05 13:53:38 +0300

    Button component added

---
+++ src/components/atoms/VButton/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-button variant="primary">{{value}}</b-button>
+</template>
+
+<script>
+export default {
+  name: "VButton",
+  props: {
+    value: {
+      type: String,
+      default: "Button"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
